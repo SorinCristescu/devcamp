ae7cd1a77fbec19239f5395efa9574226e281016
Sorin Cristescu <sorin.t.cristescu@gmail.com>
2020-05-17 19:58:52 +0200

    add advance filtering

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,13 +1,22 @@
 const ErrorResponse = require('../utils/errorResponse')
 const asyncHandler = require('../middleware/async')
+const geocoder = require('../utils/geocoder')
 const Bootcamp = require('../models/Bootcamp')
 
 
 // @description   Get all bootcamps
-// @route         GET/api/v1/bootcamps
+// @route         GET /api/v1/bootcamps
 // @access        Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-        const bootcamps = await Bootcamp.find();
+    let query;
+
+    let queryStr = JSON.stringify(req.query);
+
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+
+    query = Bootcamp.find(JSON.parse(queryStr));
+
+        const bootcamps = await query;
         res
             .status(200)
             .json({ 
@@ -19,7 +28,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 });
 
 // @description   Get single bootcamp
-// @route         GET/api/v1/bootcamps/:id
+// @route         GET /api/v1/bootcamps/:id
 // @access        Public
 exports.getBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
@@ -31,7 +40,7 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @description   Create new bootcamp
-// @route         POST/api/v1/bootcamps
+// @route         POST /api/v1/bootcamps
 // @access        Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.create(req.body);
@@ -43,7 +52,7 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @description   Update bootcamp
-// @route         PUT/api/v1/bootcamps/:id
+// @route         PUT /api/v1/bootcamps/:id
 // @access        Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
       const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
@@ -58,7 +67,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 });
 
 // @description   Delete bootcamp
-// @route         DELETE/api/v1/bootcamps/:id
+// @route         DELETE /api/v1/bootcamps/:id
 // @access        Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id); 
@@ -67,4 +76,31 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
             return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));   
         } 
         res.status(200).json({ success: true, data: {}, message: `Bootcamp ${req.params.id} deleted` })
+});
+
+// @description   Get bootcamps within a radius
+// @route         GET /api/v1/bootcamps/radius/:zipcode/:distance
+// @access        Private
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+    const { zipcode, distance } = req.params;
+
+    // Get lat/lng from geocoder
+    const loc = await geocoder.geocode(zipcode);
+    const lat = loc[0].latitude;
+    const lng = loc[0].longitude;
+
+    // Calc radius using radians
+    // Divide distance by radius of Earth
+    // Earth Radius = 3,963 mi /  6,378.1 km
+    const radius = distance / 6378;
+
+    const bootcamps = await Bootcamp.find({
+        location: { $geoWithin: { $centerSphere: [ [ lng, lat ], radius ] } }
+    })
+
+    res.status(200).json({
+        success: true,
+        count: bootcamps.length,
+        data: bootcamps
+    })
 });
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -37,27 +38,25 @@ const BootcampSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add an address']
     },
-    // location: {
-    //     // GeoJSON
-    //     type: {
-    //         type: String,
-    //         enum: ['Point'],
-    //         required: true
-    //     },
-    //     coordinates: {
-    //         type: [Number],
-    //         required: true,
-    //         index: '2dsphere'
-    //     },
-    //     formattedAddress: String,
-    //     street: String,
-    //     city: String,
-    //     state: String,
-    //     zipCode: String,
-    //     country: String,
-    // },
+    location: {
+        // GeoJSON
+        type: {
+            type: String,
+            enum: ['Point'],
+        },
+        coordinates: {
+            type: [Number],
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipCode: String,
+        country: String,
+    },
     careers: {
-        //Array of strings
+        // Array of strings
         type: [String],
         required: true,
         enum: [
@@ -102,11 +101,36 @@ const BootcampSchema = new mongoose.Schema({
 });
 
 // Create bootcamp slug from the name
-// Use normal function to have access to this keywors
+// Use normal function to have access to this keyword
 BootcampSchema.pre('save', function() {
     // console.log('Slugify run', this.name);
     this.slug = slugify(this.name, { lower: true });
     next();
 });
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    // console.log(this.address);
+    this.location = {
+        type: 'Point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+
+        // for USA
+        state: loc[0].stateCode,
+
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    }
+
+    // Do not save address in DB
+    this.address = undefined;
+
+    next();
+})
+
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
