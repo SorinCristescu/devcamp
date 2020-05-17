bd798965b1d55fc1b1e85020689e12f4713a6dae
Sorin Cristescu <sorin.t.cristescu@gmail.com>
2020-05-17 16:35:21 +0200

    updates

--- middleware/error.js
+++ middleware/error.js
@@ -24,6 +24,7 @@ const errorHandler = (err, req, res, next) => {
         const message = Object.values(err.errors).map(val => val.message);
         error = new ErrorResponse(message, 400);
     }
+    
     res.status(error.statusCode || 500).json({
         success: false,
         error: error.message || 'Server error'
