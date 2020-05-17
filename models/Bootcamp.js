21fe65de696da6a1fdc2d394401d83238e14e013
Sorin Cristescu <sorin.t.cristescu@gmail.com>
2020-05-17 17:16:52 +0200

    add slugs with slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -98,6 +99,14 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
+});
+
+// Create bootcamp slug from the name
+// Use normal function to have access to this keywors
+BootcampSchema.pre('save', function() {
+    // console.log('Slugify run', this.name);
+    this.slug = slugify(this.name, { lower: true });
+    next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
